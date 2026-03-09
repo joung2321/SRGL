47b3b911c95c9eb537e4a59b3eddeea51f162fd8
joung2321 <jounghyeon2321@gmail.com>
2026-03-10 07:29:11 +0900

    feat: add BarlineObject.cs

---
+++ addons/SRGL/graphics/BarlineObject.cs
@@ -0,0 +1,15 @@
+using Godot;
+
+using SRGL;
+using SRGL.Common;
+
+public partial class BarlineObject : NoteObject
+{
+    public override void UpdatePosition(double position, double userSpeedPxPerSec)
+    {
+        Position = _judgementPoint.Position + (float)(userSpeedPxPerSec * (_visualData.Position - position)) * Vector2.Up;
+    }
+
+    public override void SetVisualVariation(int variationIndex) {}
+    protected override void OnStateChanged(NoteState state) {}
+}
