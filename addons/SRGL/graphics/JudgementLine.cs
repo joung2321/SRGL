595a1b36d74a7073e923684a27428c2235acb881
joung2321 <jounghyeon2321@gmail.com>
2026-03-13 03:18:48 +0900

    fix: add a verification logic in JudgementLine
    - - From now on, JudgementLine verifies that
    _center and _lanes are children of itself.

--- addons/SRGL/graphics/JudgementLine.cs
+++ addons/SRGL/graphics/JudgementLine.cs
@@ -1,16 +1,26 @@
 namespace SRGL;
 
 using Godot;
+using SRGL.Common;
 
 public partial class JudgementLine : Node2D
 {
     // children of this node
     [Export] private Node2D _center;
-    [Export] private Node2D[] _lanes;
+    [Export] private Node2D[] _judgementPoints;
 
     public override void _Ready()
     {
-        // TODO: validate that _center and _lanes are children of this node
+        // verify that _center and _lanes are children of this node
+        Verifier v = new Verifier();
+
+        v.Ensure(_center.GetParent() == this, () => $"{_center.Name} is NOT a child of JudgementLine.");
+        foreach(Node2D judgementPoint in _judgementPoints)
+        {
+            v.Ensure(judgementPoint.GetParent() == this, $"{judgementPoint.Name} is NOT a child of JudgementLine.");
+        }
+
+        v.ThrowIfInvalid();
     }
     
     protected virtual int ConvertLaneIndex(int laneIndex) { return laneIndex; }
@@ -19,7 +29,7 @@ public partial class JudgementLine : Node2D
     {
         laneIndex = ConvertLaneIndex(laneIndex);
         
-        if(0 <= laneIndex && laneIndex < _lanes.Length) { return _lanes[laneIndex]; }
+        if(0 <= laneIndex && laneIndex < _judgementPoints.Length) { return _judgementPoints[laneIndex]; }
         else { return _center; }
     }
 }
