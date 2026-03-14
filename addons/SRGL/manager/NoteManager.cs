1656e3209936e094b54d37a936aaf6826b720509
joung2321 <jounghyeon2321@gmail.com>
2026-03-15 04:53:33 +0900

    perf: apply swap-and-pop to NoteManager

--- addons/SRGL/manager/NoteManager.cs
+++ addons/SRGL/manager/NoteManager.cs
@@ -12,7 +12,18 @@ public class NoteManager
     public double UserSpeedPxPerSec;
 
     private Dictionary<int, ObjectPool<NoteObject>> _notePools;
-    private Dictionary<int, NoteObject> _spawnedNotes;
+
+    // ======== NoteManager tracks spawned notes using swap-and-pop. ========
+    private const int _capacity = 512;
+    private int _count; // the number of spawned notes, index for dense array
+
+    // sparse array
+    private int[] _idToIndex = new int[_capacity]; // In dense arrays, (a index of a note whose id is x) = _idToIndex[x % _capacity].
+
+    // dense array
+    private NoteObject[] _spawnedNotes = new NoteObject[_capacity];
+    private int[] _spawnedNoteIds = new int[_capacity]; // _spawnedNoteIds[x] = hashed id of _spawnedNotes[x]
+    // ======== end ========
 
     public NoteManager(JudgementLine judgementLine, Func<int, int> selectVisualVariation)
     {
@@ -20,7 +31,13 @@ public class NoteManager
         SelectVisualVariation = selectVisualVariation;
 
         _notePools = new Dictionary<int, ObjectPool<NoteObject>>();
-        _spawnedNotes = new Dictionary<int, NoteObject>();
+
+        _count = 0;
+        for(int i=0; i<_capacity; i++)
+        {
+            _idToIndex[i] = -1;
+            _spawnedNoteIds[i] = -1;
+        }
     }
 
     /// <summary>
@@ -40,42 +57,81 @@ public class NoteManager
             no.Init(visualData, SelectVisualVariation(visualData.Lane), _judgementLine.GetJudgementPoint(visualData.Lane));
             no.SetActive(true);
 
-            _spawnedNotes[id] = no;
+            int hashedId = id % _capacity;
+            if(_idToIndex[hashedId] >= 0)
+            {
+                throw new SrglException("note id collision. use larger NoteManager._capacity");
+            }
+            else
+            {
+                _idToIndex[hashedId] = _count;
+                _spawnedNotes[_count] = no;
+                _spawnedNoteIds[_count] = hashedId;
+                _count++;
+            }
         }
     }
 
     public void UpdateNotePosition(double position)
     {
-        foreach(KeyValuePair<int, NoteObject> kvp in _spawnedNotes)
+        // linear traversal in dense array
+        for(int i=0; i<_count; i++)
         {
-            kvp.Value.UpdatePosition(position, UserSpeedPxPerSec);
+            _spawnedNotes[i].UpdatePosition(position, UserSpeedPxPerSec);
         }
     }
 
     public void ChangeNoteState(int id, NoteState state)
     {
-        if(_spawnedNotes.TryGetValue(id, out NoteObject note))
-        {
-            note.SetState(state);
-        }
+        int index = _idToIndex[id % _capacity];
+
+        if(index >= 0) { _spawnedNotes[index].SetState(state); }
     }
 
     public void DespawnNote(int id)
     {
-        if(_spawnedNotes.TryGetValue(id, out NoteObject note))
+        int hashedId = id % _capacity;
+        int index = _idToIndex[hashedId];
+
+        if(index >= 0)
         {
-            _spawnedNotes[id].InvokeReturnToPool();
-            _spawnedNotes.Remove(id);
+            // despawn note (make a hole in the dense array)
+            _idToIndex[hashedId] = -1;
+            _spawnedNotes[index].InvokeReturnToPool();
+            _spawnedNotes[index] = null;
+            _spawnedNoteIds[index] = -1;
+
+            // subtract 1 from _count (Now, _spawnedNoteIds[_count] means the backmost note in the dense array.)
+            _count--;
+
+            // fill the hole
+            if(index < _count)
+            {
+                int hashedId_back = _spawnedNoteIds[_count];
+
+                _idToIndex[hashedId_back] = index;
+                _spawnedNotes[index] = _spawnedNotes[_count];
+                _spawnedNoteIds[index] = hashedId_back;
+
+                _spawnedNotes[_count] = null;
+                _spawnedNoteIds[_count] = -1;
+            }
         }
     }
 
     public void DespawnAllNotes()
     {
-        foreach(KeyValuePair<int, NoteObject> kvp in _spawnedNotes)
+        // linear traversal in dense array
+        for(int i=0; i<_count; i++)
         {
-            kvp.Value.InvokeReturnToPool();
+            int hashedId = _spawnedNoteIds[i];
+
+            _idToIndex[hashedId] = -1;
+            _spawnedNotes[i].InvokeReturnToPool();
+            _spawnedNotes[i] = null;
+            _spawnedNoteIds[i] = -1;
         }
-        _spawnedNotes.Clear();
+        _count = 0;
     }
 
     public void Listen(JudgementQueue jq, LogicManager lm)
