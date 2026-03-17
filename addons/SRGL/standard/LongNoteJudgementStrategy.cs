a80c087ac87aba34d37fcdef54748bc4af5609c2
joung2321 <jounghyeon2321@gmail.com>
2026-03-17 09:36:03 +0900

    feat: implement LongNote

--- addons/SRGL/standard/LongNoteJudgementStrategy.cs
+++ addons/SRGL/standard/LongNoteJudgementStrategy.cs
@@ -3,7 +3,7 @@ namespace SRGL.Standard;
 using System;
 using SRGL.Common;
 
-// judgement strategy for tap note
+// judgement strategy for long note (no re-holding, no release judgement)
 public class LongNoteJudgementStrategy : IJudgementStrategy
 {
     public const int CONTEXT_HIT  = 0;
@@ -11,16 +11,93 @@ public class LongNoteJudgementStrategy : IJudgementStrategy
 
     public bool OnPress(long timeUsec, NoteLogicInstance nli, ActiveNoteTracker tracker, TimingWindow tw, Action<Judgement> invokeNoteJudged)
     {
-        throw new NotImplementedException();
+        long errorUsec = TimingWindow.CalculateErrorUsec(nli.Data.StartTimeUsec, timeUsec);
+
+        if(tracker.State == NoteState.Idle && tw.CanJudge(errorUsec))
+        {
+            tracker.State = NoteState.Holding;
+
+            Judgement j = new Judgement
+            {
+                ErrorUsec = errorUsec,
+                PartitionIndex = tw.GetPartitionIndex(errorUsec),
+                Count = 1,
+                Lane = nli.Data.Lane,
+                Context = CONTEXT_HIT
+            };
+            invokeNoteJudged(j);
+        }
+
+        return false;
     }
 
     public bool OnRelease(long timeUsec, NoteLogicInstance nli, ActiveNoteTracker tracker, TimingWindow tw, Action<Judgement> invokeNoteJudged)
     {
-        throw new NotImplementedException();
+        if(tracker.State == NoteState.Holding)
+        {
+            int remainingCombo = tracker.ConsumeMiddleTimes(long.MaxValue, nli.Data.MiddleTimesUsec);
+
+            if(remainingCombo > 0)
+            {
+                tracker.State = NoteState.Broken;
+
+                Judgement j = new Judgement
+                {
+                    ErrorUsec = tw.RadiusUsec,
+                    PartitionIndex = tw.GetLastPartitionIndex(),
+                    Count = 0,
+                    Lane = nli.Data.Lane,
+                    Context = CONTEXT_MISS
+                };
+                invokeNoteJudged(j);
+            }
+            else { tracker.State = NoteState.Released; }
+        }
+
+        return false;
     }
 
     public bool OnUpdate(long timeUsec, NoteLogicInstance nli, ActiveNoteTracker tracker, TimingWindow tw, Action<Judgement> invokeNoteJudged)
     {
-        throw new NotImplementedException();
+        // ======== start time ========
+        if(tracker.State == NoteState.Idle)
+        {
+            if(tw.IsTooLate(nli.Data.StartTimeUsec, timeUsec))
+            {
+                tracker.State = NoteState.Miss;
+
+                Judgement j = new Judgement
+                {
+                    ErrorUsec = tw.RadiusUsec,
+                    PartitionIndex = tw.GetLastPartitionIndex(),
+                    Count = 0,
+                    Lane = nli.Data.Lane,
+                    Context = CONTEXT_MISS
+                };
+                invokeNoteJudged(j);
+            }
+
+            return false;
+        }
+
+        // ======== middle times ========
+        int combo = tracker.ConsumeMiddleTimes(timeUsec, nli.Data.MiddleTimesUsec);
+
+        if(tracker.State == NoteState.Holding && combo > 0)
+        {
+            Judgement j = new Judgement
+            {
+                ErrorUsec = 0,
+                PartitionIndex = 0,
+                Count = combo,
+                Lane = nli.Data.Lane,
+                Context = CONTEXT_HIT
+            };
+            invokeNoteJudged(j);
+        }
+
+        // ======== end time ========
+        if(timeUsec >= nli.Data.EndTimeUsec) { return true; }
+        else { return false; }
     }
 }
--- addons/SRGL/standard/LongNoteObject.cs
+++ addons/SRGL/standard/LongNoteObject.cs
@@ -1,7 +1,6 @@
 namespace SRGL.Standard;
 
 using Godot;
-using System;
 using SRGL.Common;
 
 public partial class LongNoteObject : NoteObject
@@ -11,18 +10,52 @@ public partial class LongNoteObject : NoteObject
     
     protected override void OnInit(NoteVisualData visualData, int variationIndex)
     {
+        _sprite2D.Position = Vector2.Zero;
+
         _sprite2D.Centered = true;
+        _sprite2D.Offset = Vector2.Zero;
         _sprite2D.RegionEnabled = true;
         _sprite2D.RegionRect = new Rect2(0, 0, _sprite2D.Texture.GetWidth(), _sprite2D.Texture.GetHeight());
 
-        if(0 <= variationIndex && variationIndex < _textures.Length)
+        if(0 <= variationIndex && variationIndex < _textures.Length) { _sprite2D.Texture = _textures[variationIndex]; }
+        _sprite2D.Modulate = new Color(0.9f,0.9f,0.9f);
+    }
+
+    protected override void OnStateChanged()
+    {
+        switch(_state)
         {
-            _sprite2D.Texture = _textures[variationIndex];
+            case NoteState.Idle:
+            case NoteState.Released:
+            _sprite2D.Modulate = new Color(0.9f,0.9f,0.9f);
+            break;
+
+            case NoteState.Holding:
+            _sprite2D.Modulate = new Color(1,1,1);
+            break;
+
+            case NoteState.Miss:
+            case NoteState.Broken:
+            _sprite2D.Modulate = new Color(0.5f,0.5f,0.5f,0.5f);
+            break;
         }
     }
 
     public override void UpdatePosition(double position, double userSpeedPxPerSec)
     {
-        throw new NotImplementedException();
+        _sprite2D.Scale = new Vector2(1, (float)(_visualData.Length * userSpeedPxPerSec / _sprite2D.Texture.GetHeight()));
+
+        if(_state == NoteState.Idle || _state == NoteState.Miss)
+        {
+            Position = _judgementPoint.Position + (float)(userSpeedPxPerSec * (_visualData.Position - position + _visualData.Length / 2)) * Vector2.Up;
+        }
+        else
+        {
+            double remainingLength = _visualData.Position + _visualData.Length - position;
+            float h = (float)(_sprite2D.Texture.GetHeight() * remainingLength / _visualData.Length);
+
+            _sprite2D.RegionRect = new Rect2(0, 0, _sprite2D.Texture.GetWidth(), h);
+            Position = _judgementPoint.Position + (float)(userSpeedPxPerSec * (remainingLength / 2)) * Vector2.Up;
+        }
     }
 }
